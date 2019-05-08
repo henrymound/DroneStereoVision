c3dd6f75f9da4fcfad70f8ad61bbb71659033362
Henry Mound <henrymound@gmail.com>
2019-05-08 15:07:49 -0400

    Adding line 1

---
+++ Stitching/README.md
@@ -0,0 +1,2 @@
+# Things To Try
+- When using scanning stitching the difference between the iamges in a line may not be significant enough. Perhaps flying up and down will yield better results
--- Stitching/stitch_read/lineread.py
+++ Stitching/stitch_read/lineread.py
@@ -6,7 +6,7 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy as np
 import imutils
 
-NUM_IMAGES = 10
+NUM_IMAGES = 25
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 
@@ -14,12 +14,12 @@ stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 if __name__ == '__main__':
 	# Read a series of NUM_IMAGES images
 	for i in range(0, NUM_IMAGES):
-		img = cv2.imread("../line_flights/line3/"+str(i)+".png",cv2.IMREAD_COLOR)
+		img = cv2.imread("../line_flights/line2/"+str(i)+".png",cv2.IMREAD_COLOR)
 		images.append(img) # Append the image and stitch
 		(status, stitched) = stitcher.stitch(images)
 		print("Added image " + str(i) + " with status: " + str(status))
 		if status == 0: # If image is valid
-			cv2.imwrite('../line_flights/line3/results/stitched'+str(i)+'.png', stitched) # Save image
+			cv2.imwrite('../line_flights/line2/results/stitched'+str(i)+'.png', stitched) # Save image
 			#cv2.imshow('Stitched Image', stitched)
 			cv2.waitKey(1)
 	cv2.imshow('Stitched Image', stitched)
