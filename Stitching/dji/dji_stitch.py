9d562d1b21cffe0020aa2961ce12ecc4adb97c1a
Henry Mound <henrymound@gmail.com>
2019-05-09 17:14:03 -0400

    Updated results

--- Stitching/dji/dji_stitch.py
+++ Stitching/dji/dji_stitch.py
@@ -6,7 +6,7 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy as np
 import imutils
 
-NUM_IMAGES = 100#910-433
+NUM_IMAGES = 910-433
 START_NUM = 433
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
@@ -14,9 +14,10 @@ stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 
 if __name__ == '__main__':
 
-	for i in range(0, NUM_IMAGES):
+	for i in range(0, NUM_IMAGES, 2): # Only add every other image
 		imgPath = "flight2/DJI_0"+str(i + START_NUM)+".JPG"
-		imgToAppend = cv2.imread(imgPath, cv2.IMREAD_COLOR) 
+		img = cv2.imread(imgPath, cv2.IMREAD_COLOR)
+		imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale by 50%
 		images.append(imgToAppend) 
 		print("Appended: " + imgPath)
 	
