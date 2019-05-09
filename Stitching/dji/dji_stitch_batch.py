5e59d97a85c3d5eb9012a2caed09587736d64f68
Henry Mound <henrymound@gmail.com>
2019-05-09 17:31:01 -0400

    Added more batch images

--- Stitching/dji/dji_stitch_batch.py
+++ Stitching/dji/dji_stitch_batch.py
@@ -8,7 +8,7 @@ import imutils
 
 NUM_IMAGES = 910-433
 START_NUM = 433
-BATCH_SIZE = 20
+BATCH_SIZE = 10
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 
@@ -18,13 +18,14 @@ if __name__ == '__main__':
 	print("Stitching " + str(NUM_IMAGES) + " with a batch size of " + str(BATCH_SIZE))
 	for i in range(0, NUM_IMAGES):
 		imgPath = "flight2/DJI_0"+str(i + START_NUM)+".JPG"
-		imgToAppend = cv2.imread(imgPath, cv2.IMREAD_COLOR) 
+		img = cv2.imread(imgPath, cv2.IMREAD_COLOR) 
+		imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale to 30%
 		images.append(imgToAppend) 
 		print("Appended: " + imgPath)
 		if(len(images) == BATCH_SIZE):
 			print("Batch " + str(c) + " Complete: Saving in batch/"+str(c)+".JPG")
 			(status, stitched) = stitcher.stitch(images)
-			cv2.imwrite('batch/'+str(c)+'.JPG',stitched)
+			cv2.imwrite('batch_downscaled/'+str(c)+'.JPG',stitched)
 			cv2.waitKey(0)
 			images = []
 			c+=1
