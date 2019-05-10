c4b00b9ce04d74bab867e8e8ab8083e55acdb6ef
Henry Mound <henrymound@gmail.com>
2019-05-10 14:56:41 -0400

    Added downscale 2

--- Stitching/dji/batch_downscaled_10/dji_stitch_nested.py
+++ Stitching/dji/batch_downscaled_10/dji_stitch_nested.py
@@ -6,29 +6,26 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy as np
 import imutils
 
-NUM_IMAGES = 910-433
-START_NUM = 433
-BATCH_SIZE = 2
+NUM_IMAGES = 47
+START_NUM = 0
+#BATCH_SIZE = 30
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 
 
 if __name__ == '__main__':
-	c = 0 # Count the batch num
-	print("Stitching " + str(NUM_IMAGES) + " with a batch size of " + str(BATCH_SIZE))
+	print("Stitching all " + str(NUM_IMAGES) + "batch images")
 	for i in range(0, NUM_IMAGES):
-		imgPath = "flight2/DJI_0"+str(i + START_NUM)+".JPG"
+		imgPath = str(i + START_NUM)+".JPG"
 		img = cv2.imread(imgPath, cv2.IMREAD_COLOR) 
 		imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale to 30%
 		images.append(imgToAppend) 
 		print("Appended: " + imgPath)
-		if(len(images) == BATCH_SIZE):
-			print("Batch " + str(c) + " Complete: Saving in batch_downscaled_30/"+str(c)+".JPG")
-			(status, stitched) = stitcher.stitch(images)
-			cv2.imwrite('batch_downscaled_2/'+str(c)+'.JPG',stitched)
-			cv2.waitKey(0)
-			images = []
-			c+=1
+	print("STITCING ALL IN ARRAY")	
+	(status, stitched) = stitcher.stitch(images)
+	cv2.imwrite('result.JPG',stitched)
+	cv2.waitKey(0)
+	
 	#cv2.imshow('Stitched Image', stitched)
 	
 	#cv2.waitKey(0)
