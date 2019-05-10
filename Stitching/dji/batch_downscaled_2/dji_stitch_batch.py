f18c3e955492d02f494dfeb191be8da076b293b9
Henry Mound <henrymound@gmail.com>
2019-05-10 15:00:46 -0400

    Continued experimenting

--- Stitching/dji/batch_downscaled_2/dji_stitch_batch.py
+++ Stitching/dji/batch_downscaled_2/dji_stitch_batch.py
@@ -6,8 +6,7 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy as np
 import imutils
 
-NUM_IMAGES = 910-433
-START_NUM = 433
+NUM_IMAGES = 236
 BATCH_SIZE = 2
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
@@ -17,13 +16,13 @@ if __name__ == '__main__':
 	c = 0 # Count the batch num
 	print("Stitching " + str(NUM_IMAGES) + " with a batch size of " + str(BATCH_SIZE))
 	for i in range(0, NUM_IMAGES):
-		imgPath = "flight2/DJI_0"+str(i + START_NUM)+".JPG"
+		imgPath = str(i + 2)+".JPG"
 		img = cv2.imread(imgPath, cv2.IMREAD_COLOR) 
-		imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale to 30%
-		images.append(imgToAppend) 
+		#imgToAppend = cv2.resize(img,None,fx=0.9,fy=0.9) # Downscale to 50%
+		images.append(img) 
 		print("Appended: " + imgPath)
 		if(len(images) == BATCH_SIZE):
-			print("Batch " + str(c) + " Complete: Saving in batch_downscaled_30/"+str(c)+".JPG")
+			print("Batch " + str(c) + " Complete: Saving in batch_downscaled_2/"+str(c)+".JPG")
 			(status, stitched) = stitcher.stitch(images)
 			cv2.imwrite('batch_downscaled_2/'+str(c)+'.JPG',stitched)
 			cv2.waitKey(0)
