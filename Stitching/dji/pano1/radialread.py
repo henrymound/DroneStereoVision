91db6d55b10131bcecc01dd5ede74ee6caef1638
Henry Mound <henrymound@gmail.com>
2019-05-12 15:41:12 -0400

    Stitching DJI Pano 1 and 2

---
+++ Stitching/dji/pano1/radialread.py
@@ -0,0 +1,30 @@
+import sys
+import traceback
+import threading
+import av
+import cv2.cv2 as cv2  # for avoidance of pylint error
+import numpy as np
+import imutils
+
+NUM_IMAGES = 393-366
+STARTNUM = 366
+images = []
+stitcher = cv2.Stitcher_create(cv2.Stitcher_PANORAMA)
+
+
+if __name__ == '__main__':
+	# Read a series of NUM_IMAGES images
+	for i in range(0, NUM_IMAGES):
+		img = cv2.imread("DJI_0"+str(i+STARTNUM)+".JPG",cv2.IMREAD_COLOR)
+		images.append(img) # Append the image and stitch
+		print("Added image " + str(i))
+	print("STITCHING")
+	(status, stitched) = stitcher.stitch(images)
+	if status == 0: # If image is valid
+		#cv2.imshow('Stitched Image', stitched)
+		cv2.imwrite("RESULTS.JPG", stitched)
+		cv2.waitKey(1)
+		
+	cv2.waitKey(0)
+	#(status, stitched) = stitcher.stitch(images)
+	cv2.destroyAllWindows()
