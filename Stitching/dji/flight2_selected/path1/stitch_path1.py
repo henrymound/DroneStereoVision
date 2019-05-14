b6932b25399586a19ccd9203fb9c9b03a94a5c4f
Henry Mound <henrymound@gmail.com>
2019-05-13 22:24:36 -0400

    Continued editing path1

--- Stitching/dji/flight2_selected/path1/stitch_path1.py
+++ Stitching/dji/flight2_selected/path1/stitch_path1.py
@@ -6,23 +6,23 @@ import cv2.cv2 as cv2  # for avoidance of pylint error
 import numpy as np
 import imutils
 
-NUM_IMAGES = 910-433
-START_NUM = 433
+NUM_IMAGES = 4
+START_NUM = 1
 images = []
 stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
 
 
 if __name__ == '__main__':
 
-	for i in range(0, NUM_IMAGES, 2): # Only add every other image
-		imgPath = "flight2/DJI_0"+str(i + START_NUM)+".JPG"
+	for i in range(0, NUM_IMAGES, 1): # Only add every other image
+		imgPath = str(i + START_NUM)+".JPG"
 		img = cv2.imread(imgPath, cv2.IMREAD_COLOR)
-		imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale by 50%
-		images.append(imgToAppend) 
+		#imgToAppend = cv2.resize(img,None,fx=0.3,fy=0.3) # Downscale to 30% of original
+		images.append(img) 
 		print("Appended: " + imgPath)
 	
 	(status, stitched) = stitcher.stitch(images)
 	#cv2.imshow('Stitched Image', stitched)
-	cv2.imwrite('result.JPG',stitched)
+	cv2.imwrite('FULL4.JPG',stitched)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
